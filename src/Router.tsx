b21187642addab4c594336e5b1c181a632a8a906
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-05-25 23:35:55 +0530

    fixed loading animation

--- src/Router.tsx
+++ src/Router.tsx
@@ -181,7 +181,11 @@ function Router() {
 		}
 	]);
 
-	if (loading) return <img className="loading" src="/loading.gif" alt="loading" />;
+	if (loading) return (
+		<svg className='loading' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="200" height="200" xmlnsXlink="http://www.w3.org/1999/xlink">
+			<path strokeWidth="12" stroke="#2563EB" fill="none" d="M50 15A35 35 0 1 0 74.74873734152916 25.251262658470843"></path>
+			<path fill="#2563EB" d="M49 3L49 27L61 15L49 3"></path>
+		</svg>);
 
 	return (
 		<>
